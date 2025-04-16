dc23dc3589409037e783431330ce1548a086dd4c
Hagb Green (Guo Junyu) <hagb@hagb.name>
2025-04-16 20:20:48 +0800

    add existence of groebner basis and `groebner_basis_rem_eq_zero_iff` with sorry

---
+++ Groebner/Ideal.lean
@@ -0,0 +1,14 @@
+import Mathlib.RingTheory.Noetherian.Basic
+import Mathlib.RingTheory.Ideal.Span
+import Mathlib.RingTheory.Finiteness.Basic
+
+
+namespace Ideal
+open Ideal
+variable {R : Type _} [Semiring R]
+
+theorem fg_span_iff_fg_span_finset_subset (s : Set R) :
+  (span s).FG ↔ ∃ (s' : Finset R), s'.toSet ⊆ s ∧ span s = span s' := by
+  sorry
+
+end Ideal
