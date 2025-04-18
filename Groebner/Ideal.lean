7e25959bd005b1dc5d587d061b8401b7622a1e05
tsuki8 <3118181069@qq.com>
2025-04-18 19:44:45 +0800

    store some message

--- Groebner/Ideal.lean
+++ Groebner/Ideal.lean
@@ -1,6 +1,8 @@
+import Mathlib --remove later
 import Mathlib.RingTheory.Noetherian.Basic
 import Mathlib.RingTheory.Ideal.Span
 import Mathlib.RingTheory.Finiteness.Basic
+
 
 
 namespace Ideal
@@ -9,6 +11,6 @@ variable {R : Type _} [Semiring R]
 
 theorem fg_span_iff_fg_span_finset_subset (s : Set R) :
   (span s).FG ↔ ∃ (s' : Finset R), s'.toSet ⊆ s ∧ span s = span s' := by
-  sorry
+    sorry
 
 end Ideal
