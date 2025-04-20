d92d4bcf47da8c2bd917c8a19c67ca69bf528230
tsuki <tsuki@tsuki-2.local>
2025-04-20 16:13:00 +0800

    add some lemma Apr20

--- Groebner/Ideal.lean
+++ Groebner/Ideal.lean
@@ -33,16 +33,6 @@ variable (G': Finset (MvPolynomial σ k)) (G'': Set (MvPolynomial σ k))
 variable (I I₁ I₂ : Ideal (MvPolynomial σ k))
 variable {R : Type _} [CommSemiring R]
 
--- @[simp]
-lemma a (c : k) : MvPolynomial.monomial s c =
-  MvPolynomial.monomial 0 c * MvPolynomial.monomial s 1 := by
-  -- rw_search
-  sorry
-  -- rw [@MvPolynomial.monomial_zero', @MvPolynomial.C_mul_monomial, @NonAssocRing.mul_one]
-
-open Finsupp
-
-
 lemma leading_term_ideal_span_monomial : span (m.leadingTerm '' G') =
   span ((fun p => MvPolynomial.monomial (m.degree p) (1 : k)) '' (G'' \ {0})) := by
   -- simp [MonomialOrder.leadingTerm]
