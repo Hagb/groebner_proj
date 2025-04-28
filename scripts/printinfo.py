78e970f2b2c33d1421fae084cec81e9957f8f394
Hagb (Junyu Guo) <hagb@hagb.name>
2025-04-28 17:16:58 +0800

    fix latex build

--- scripts/printinfo.py
+++ scripts/printinfo.py
@@ -78,19 +78,21 @@ def generate_latex_content(json_data):
           processed_uses = (to_label(u) for u in proof_uses)
           proof_uses_content = "\\uses{" + ", ".join(processed_uses) + "}"
 
+        _backdash = "\\" # python3.10 workaround
+
         # \settitle command requires a patched version of plastexdepgraph:
         # https://github.com/WuProver/plastexdepgraph/tree/settitle.
         # it can be installed via
         # `pip install --force-reinstall https://github.com/WuProver/plastexdepgraph/archive/refs/heads/settitle.zip`
         content = f"""
-        \\begin{{definition}}[{{{to_label(name)}}}]\\label{{{to_label(name)}}}
+        \\begin{{definition}}[{to_label(name).replace('_', _backdash+'_')}]\\label{{{to_label(name)}}}
           \\settitle{{{to_title(name)}}}
           {lean_ref}
           {statement_uses_content}
           {statement_lean_ok}
           {docstring if docstring else ""}
         \\end{{definition}}""" if not is_thm else f"""
-        \\begin{{lemma}}[{{{to_label(name)}}}]\\label{{{to_label(name)}}}
+        \\begin{{lemma}}[{to_label(name).replace('_', _backdash+'_')}]\\label{{{to_label(name)}}}
           \\settitle{{{to_title(name)}}}
           {lean_ref}
           {statement_uses_content}
